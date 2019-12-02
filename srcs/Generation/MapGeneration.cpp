a5ae9c8a26f2e4e37a0e45c14bc1a547b29cbbd4
Maksim <prokushevmaksim@gmail.com>
2019-12-02 22:04:20 +0300

    fix bugs

--- srcs/Generation/MapGeneration.cpp
+++ srcs/Generation/MapGeneration.cpp
@@ -1,13 +1,13 @@
-#include "Map.h"
+#include "./Generation/Map.h"
 
 class MapGeneration
 {
-    template <typename T> inline T Lerp(const T &v0, const T &v1, float t)
+    template <typename T> inline T _Lerp(const T &v0, const T &v1, float t)
     {
-        return v0 + (v1-v0)*std::max(0.f, std::min(1.f, t));
+        return v0 + (v1 - v0) * std::max(0.f, std::min(1.f, t));
     }
 
-    float Hash(const float n)
+    float _Hash(const float n)
     {
         float x = sin(n) * 43758.5453f;
         return x - floor(x);
@@ -19,12 +19,12 @@ class MapGeneration
         glm::vec3 f(x.x - p.x, x.y - p.y, x.z - p.z);
         f = f * (f * (glm::vec3(3.f, 3.f, 3.f) - f * 2.f));
         float n = glm::dot(p, glm::vec3(1.f, 57.f, 113.f));
-        return Lerp(Lerp(
-                        Lerp(Hash(n +  0.f), Hash(n +  1.f), f.x),
-                        Lerp(Hash(n + 57.f), Hash(n + 58.f), f.x), f.y),
-                    Lerp(
-                        Lerp(Hash(n + 113.f), Hash(n + 114.f), f.x),
-                        Lerp(Hash(n + 170.f), Hash(n + 171.f), f.x), f.y), f.z);
+        return _Lerp(_Lerp(
+                        _Lerp(_Hash(n +  0.f), _Hash(n +  1.f), f.x),
+                        _Lerp(_Hash(n + 57.f), _Hash(n + 58.f), f.x), f.y),
+                    _Lerp(
+                        _Lerp(_Hash(n + 113.f), _Hash(n + 114.f), f.x),
+                        _Lerp(_Hash(n + 170.f), _Hash(n + 171.f), f.x), f.y), f.z);
     }
 
     void Generation(float height, float width, std::unordered_map<glm::ivec2, StoredMapData*> umap)
