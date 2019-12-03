70f1601d76761048823eb658b7a0df9022391b41
Maksim <prokushevmaksim@gmail.com>
2019-12-03 18:09:02 +0300

    added biomedefine

--- srcs/entry.cpp
+++ srcs/entry.cpp
@@ -1,6 +1,7 @@
 #include "Base.h"
 #include "Engine/Engine.h"
 #include "Engine/Locator.hpp"
+#include "Generation/Map.h"
 
 void testGeometry();
 
@@ -8,8 +9,17 @@ void testGeometry();
 int	main() {
 	Game game = Game();
 	// Locator::getLogger()->Log("Hi");
-	game.GameLoop();
-	// std::cout << "Hello world!\n";
-	// Geometry* g = new Geometry(std::string("resources/Models/Box.obj"));
+	// game.GameLoop();
+    // Geometry* g = new Geometry(std::string("resources/Models/Box.obj"));
+//	std::unordered_map<glm::ivec2, StoredMapData*> umap;
+//	MapGeneration* mp = new MapGeneration;
+//	mp->Generation(16.f, 16.f, umap);
+	std::cout << "Hello world!\n";
+//	for (auto pair : umap)
+//	{
+		// if (pair.second->elevation <= 0.4f)
+//			std::cout << "pos " << pair.first.x << " " << pair.first.y << ": " << pair.second->elevation << std::endl;
+//			std::cout << pair.second->biom << std::endl;
+//	}
 	return (0);
 }
