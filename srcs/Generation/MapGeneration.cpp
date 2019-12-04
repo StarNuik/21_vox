813b67af5ca3eda4e8556dd92ed6a8d2ad21159d
Maksim <prokushevmaksim@gmail.com>
2019-12-04 17:45:58 +0300

    OpenSimplexNoise used

--- srcs/Generation/MapGeneration.cpp
+++ srcs/Generation/MapGeneration.cpp
@@ -48,12 +48,12 @@ float MapGeneration::Noise(const glm::vec3 &x)
     glm::vec3 f(x.x - p.x, x.y - p.y, x.z - p.z);
     f = f * (f * (glm::vec3(3.f, 3.f, 3.f) - f * 2.f));
     float n = glm::dot(p, glm::vec3(1.f, 57.f, 113.f));
-    return (LERP(LERP(
+    return LERP(LERP(
                     LERP(_Hash(n +  0.f), _Hash(n +  1.f), f.x),
                     LERP(_Hash(n + 57.f), _Hash(n + 58.f), f.x), f.y),
                 LERP(
                     LERP(_Hash(n + 113.f), _Hash(n + 114.f), f.x),
-                    LERP(_Hash(n + 170.f), _Hash(n + 171.f), f.x), f.y), f.z)) / 2.f + 0.5f;
+                    LERP(_Hash(n + 170.f), _Hash(n + 171.f), f.x), f.y), f.z);
 }
 
 MapGeneration::StoredMapData  MapGeneration::HighLandGenerationColumn(glm::ivec2 pos, glm::ivec2 blockPosition)
