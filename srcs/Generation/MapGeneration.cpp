d8b3d1bf3a047ab62acb5dff0782c5a69bcc6b44
Maksim <prokushevmaksim@gmail.com>
2019-12-04 17:45:58 +0300

    OpenSimplexNoise used

--- srcs/Generation/MapGeneration.cpp
+++ srcs/Generation/MapGeneration.cpp
@@ -40,18 +40,20 @@ float MapGeneration::_Hash(const float n)
     return x - floor(x);
 }
 
+
+
 float MapGeneration::Noise(const glm::vec3 &x)
 {
     glm::vec3 p(floor(x.x), floor(x.y), floor(x.z));
     glm::vec3 f(x.x - p.x, x.y - p.y, x.z - p.z);
     f = f * (f * (glm::vec3(3.f, 3.f, 3.f) - f * 2.f));
     float n = glm::dot(p, glm::vec3(1.f, 57.f, 113.f));
-    return (LERP(LERP(
+    return LERP(LERP(
                     LERP(_Hash(n +  0.f), _Hash(n +  1.f), f.x),
                     LERP(_Hash(n + 57.f), _Hash(n + 58.f), f.x), f.y),
                 LERP(
                     LERP(_Hash(n + 113.f), _Hash(n + 114.f), f.x),
-                    LERP(_Hash(n + 170.f), _Hash(n + 171.f), f.x), f.y), f.z)) / 2.f + 0.5f;
+                    LERP(_Hash(n + 170.f), _Hash(n + 171.f), f.x), f.y), f.z);
 }
 
 void MapGeneration::Generation(float height, float width, std::unordered_map<glm::ivec2, StoredMapData*> &umap)
@@ -62,23 +64,21 @@ void MapGeneration::Generation(float height, float width, std::unordered_map<glm
         {
             float nx = x / width - 0.5f, ny = y / height - 0.5f;
             glm::ivec2 pos = glm::ivec2(x, y);
-            float e = (1.0f * Noise(glm::vec3(1.f * nx, 1.f * ny, 1.f))
-              + 0.50f * Noise(glm::vec3(2.f * nx, 2.f * ny, 1.f))
-              + 0.25f * Noise(glm::vec3(4.f * nx, 4.f * ny, 1.f))
-              + 0.13f * Noise(glm::vec3(8.f * nx, 8.f * ny, 1.f))
-              + 0.06f * Noise(glm::vec3(16.f * nx, 16.f * ny, 1.f))
-              + 0.03f * Noise(glm::vec3(32.f * nx, 32.f * ny, 1.f)));
-            float m = (1.0f * Noise(glm::vec3(1.f * nx, 1.f * ny, 1.f))
-              + 0.75f * Noise(glm::vec3(2.f * nx, 2.f * ny, 1.f))
-              + 0.33f * Noise(glm::vec3(4.f * nx, 4.f * ny, 1.f))
-              + 0.33f * Noise(glm::vec3(8 * nx, 8 * ny, 1.f))
-              + 0.33f * Noise(glm::vec3(16.f * nx, 16.f * ny, 1.f))
-              + 0.50f * Noise(glm::vec3(32.f * nx, 32.f * ny, 1.f)));
+            float e = (1.0f * SimplexNoise::noise(1.f * nx, 1.f * ny)
+              + 0.50f * SimplexNoise::noise(2.f * nx, 2.f * ny)
+              + 0.25f * SimplexNoise::noise(4.f * nx, 4.f * ny)
+              + 0.13f * SimplexNoise::noise(8.f * nx, 8.f * ny)
+              + 0.06f * SimplexNoise::noise(16.f * nx, 16.f * ny)
+              + 0.03f * SimplexNoise::noise(32.f * nx, 32.f * ny));
+            float m = (1.0f * SimplexNoise::noise(1.f * nx, 1.f * ny)
+              + 0.75f * SimplexNoise::noise(2.f * nx, 2.f * ny)
+              + 0.33f * SimplexNoise::noise(4.f * nx, 4.f * ny)
+              + 0.33f * SimplexNoise::noise(8 * nx, 8 * ny)
+              + 0.33f * SimplexNoise::noise(16.f * nx, 16.f * ny)
+              + 0.50f * SimplexNoise::noise(32.f * nx, 32.f * ny, 1.f));
             m /= (1.0f + 0.75f + 0.33f + 0.33f + 0.33f + 0.50f);
-            // float e = 1 * Noise(glm::vec3(1.f * nx, 1.f * ny, 1.f));
-            // e += 0.5 * Noise(glm::vec3(2.f * nx, 2.f * ny, 1.f));
-            // e += 0.25 * Noise(glm::vec3(4.f * nx, 4.f * ny, 1.f));
-            umap[pos] = new StoredMapData(pow(e, EXP)); // резкие горные пики
+            umap[pos] = new StoredMapData(pow(e, EXP) - 0.42); // резкие горные пики
+            // std::cout << umap[pos]->elevation << std::endl;
             // umap[pos] = new StoredMapData(pow(e, EXP));
             umap[pos]->biom = BiomeDefinition(e, m);
         }
@@ -94,9 +94,8 @@ void MapGeneration::SpawnObject(Game *game, std::unordered_map<glm::ivec2, Store
     {
         RenderModel* m = new RenderModel(r->GetShader("Base"), r->GetTexture("Stone"), r->GetGeometry("Box"));
         _game->GetRenderer()->AddModel(m);
-        m->SetPosition(glm::vec3(pair.first.x, floorf(pair.second->elevation * 7.f), pair.first.y));
-        std::cout << "| x: " << pair.first.x << "| y: " << pair.first.y << "| z: " << pair.second->elevation << std::endl;
+        m->SetPosition(glm::vec3(pair.first.x, floorf(pair.second->elevation * 7.f) - 15, pair.first.y));
+        // if (pair.second->elevation > 0.9 || pair.second->elevation < -0.0000001)
+          // std::cout << "| x: " << pair.first.x << "| y: " << pair.first.y << "| z: " << pair.second->elevation << std::endl;
     }
-    // m->SetPosition(glm::vec3(umap))
 }
-
--- srcs/Generation/SpawnObject.cpp
+++
@@ -1,2 +0,0 @@
-#include "Generation/SpawnObject.h"
-
