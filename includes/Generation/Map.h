b3de3a1ebe207d968b0ba97d3bf9ead2845e3852
Maksim <prokushevmaksim@gmail.com>
2019-12-04 17:45:14 +0300

    added OpenSimplexNoise lib, and add 'random' method to 'MapGeneration' class

--- includes/Generation/Map.h
+++ includes/Generation/Map.h
@@ -12,8 +12,10 @@
 
 #include "BiomeDefine.h"
 #include "Engine/Engine.h"
+#include <cstdint>
+#include "OpenSimplexNoise/OpenSimplexNoise.h"
 #define FREQ 1
-#define EXP 1.5
+#define EXP 0.6
 
 class StoredMapData
 {
@@ -27,20 +29,21 @@ class StoredMapData
 class MapGeneration
 {
     public:
+        int BiomeDefinition(float e, float m);
 
-    int BiomeDefinition(float e, float m);
+        float random (glm::vec2 st) 
+        {
+            return glm::fract(sin(glm::dot(st, glm::vec2(12.9898, 78.233))) * 43758.5453123);
+        }
 
-    template <typename T> inline T _Lerp(const T &v0, const T &v1, float t)
-    {
-        return v0 + (v1 - v0) * std::max(0.f, std::min(1.f, t));
-    };
-
-    float _Hash(const float n);
-
-    float Noise(const glm::vec3 &x);
-
-    void Generation(float height, float width, std::unordered_map<glm::ivec2, StoredMapData*> &umap);
-
-    void SpawnObject(Game *game, std::unordered_map<glm::ivec2, StoredMapData*> umap);
+        template <typename T> inline T _Lerp(const T &v0, const T &v1, float t)
+        {
+            return v0 + (v1 - v0) * std::max(0.f, std::min(1.f, t));
+        };
+        
+        float _Hash(const float n);
+        float Noise(const glm::vec3 &x);
+        void Generation(float height, float width, std::unordered_map<glm::ivec2, StoredMapData*> &umap);
+        void SpawnObject(Game *game, std::unordered_map<glm::ivec2, StoredMapData*> umap);
 };
 
