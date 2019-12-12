0ccb721ee81d24cbcfd7372010f9710a5942573f
StarNuik <starnuik@gmail.com>
2019-12-12 20:39:42 +0300

    setting up ui

--- includes/UI/UI.h
+++ includes/UI/UI.h
@@ -1,31 +1,52 @@
 #pragma once
 
 #include "Types.h"
+#include <deque>
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include "Engine/Engine.h"
 
-class Game;
+#define UI_PLOT_FRAMES 100
+
+struct DataRendering {
+	DataRendering();
+	std::deque<uint> frameTimes;
+	float maxFrameTime;
+};
+
+struct DataPlayer {};
+struct DataGeneration {};
+struct DataResources {};
 
 class UIController {
 public:
 	UIController(Game*);
 	~UIController();
 	void Draw();
+	void UpdateData();
 	void SetState(bool);
 	bool GetState();
 	void SetPlayer(Player*);
 private:
-	void StartFrame();
-	void EndFrame();
 	void MainUI();
+
+	void UpdatePlayer();
+	void UpdateRendering();
+	void UpdateGeneration();
+	void UpdateResources();
+
 	void PlayerUI();
 	void RenderingUI();
 	void GenerationUI();
 	void ResourcesUI();
+
 	Player* _player;
 	ResourceLoader* _resources;
 	Game* _game;
 	bool _uiOn;
+	DataRendering _dataRendering;
+	DataPlayer _dataPlayer;
+	DataGeneration _dataGeneration;
+	DataResources _dataResources;
 };
--- srcs/UI/PlayerUI.cpp
+++ srcs/UI/PlayerUI.cpp
@@ -1,5 +1,14 @@
 #include "UI/UI.h"
 
+// DataPlayer::DataPlayer() {}
+
+void UIController::UpdatePlayer() {
+	DataPlayer& data = _dataPlayer;
+
+};
+
 void UIController::PlayerUI() {
-	
+	DataPlayer& data = _dataPlayer;
+
+	ImGui::Text("Player UI");
 };
