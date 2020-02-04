e24828855a665df877effd0cb27a277dae1e466c
Maksim <prokushevmaksim@gmail.com>
2020-02-04 20:08:50 +0300

    Now tree generation is hardcode

---
+++ includes/Generation/TreeGeneration.h
@@ -0,0 +1,67 @@
+#include "MapGeneration.h"
+#include "World/Block.h"
+
+#define TREE_HEIGHT 6
+#define TREE_SIZE 5 //x/z size
+
+enum TreeType {
+    BasicTree = 0,
+    First = BasicTree,
+    Last = BasicTree,
+    Size = Last + 1
+};
+
+__BLOCK_TYPE air = Block::Air;
+__BLOCK_TYPE lg = Block::Log;
+__BLOCK_TYPE lvs = Block::Leaves;
+
+__BLOCK_TYPE TreeModels[TreeType::Size][TREE_HEIGHT][TREE_SIZE][TREE_SIZE] =
+{
+    //--------------------------------------------------------------
+    //BasicTree
+    {
+        { // 0
+            {air, air, air, air, air},
+            {air, air, air, air, air},
+            {air, air, lg, air, air},
+            {air, air, air, air, air},
+            {air, air, air, air, air},
+        },
+        { // 1
+            {air, air, air, air, air},
+            {air, air, air, air, air},
+            {air, air, lg, air, air},
+            {air, air, air, air, air},
+            {air, air, air, air, air},
+        },
+        { // 2
+            {air, lvs, lvs, lvs, air},
+            {lvs, lvs, lvs, lvs, lvs},
+            {lvs, lvs, lg, lvs, lvs},
+            {lvs, lvs, lvs, lvs, lvs},
+            {air, lvs, lvs, lvs, air},
+        },
+        { // 3
+            {air, lvs, lvs, lvs, air},
+            {lvs, lvs, lvs, lvs, lvs},
+            {lvs, lvs, lg, lvs, lvs},
+            {lvs, lvs, lvs, lvs, lvs},
+            {air, lvs, lvs, lvs, air},
+        },
+        { // 4
+            {air, lvs, lvs, lvs, air},
+            {lvs, lvs, lvs, lvs, lvs},
+            {lvs, lvs, lg, lvs, lvs},
+            {lvs, lvs, lvs, lvs, lvs},
+            {air, lvs, lvs, lvs, air},
+        },
+        { // 5
+            {air, air, air, air, air},
+            {air, lvs, lvs, lvs, air},
+            {air, lvs, lvs, lvs, air},
+            {air, lvs, lvs, lvs, air},
+            {air, air, air, air, air},
+        }
+    }
+    //--------------------------------------------------------------
+};
